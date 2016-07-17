c3cff4a0e479d7a71aed1ba0409e65a0287ee5ce
Łukasz Piotr Łuczak <lukasz.luczak@onet.pl>
2016-07-17 12:37:11 +0200

    [L03] Point class

--- app/application.ts
+++ app/application.ts
@@ -1,46 +1,52 @@
+class Point {
+    x: number;
+    y: number;
+    constructor(x : number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(point: Point) {
+        this.x += point.x;
+        this.y += point.y;
+    }
+}
+
 class Ball {
     radius : number;
-    posX : number;
-    posY : number;
-    dirX : number;
-    dirY : number; 
-    minX : number;
-    minY : number;
-    maxX : number;
-    maxY : number;
+    pos  : Point;
+    dir  : Point;
+    min  : Point;
+    max  : Point;
 
     constructor(radius : number, posX : number, posY : number, dirX : number, dirY : number) {
         this.radius = radius;
-        this.posX = posX;
-        this.posY = posY;
-        this.dirX = dirX;
-        this.dirY = dirY;
+        this.pos = new Point(posX, posY);
+        this.dir = new Point(dirX, dirY);        
     }
 
-    move() {
-        if (this.posX + this.dirX <= this.minX) {
-            this.dirX *= -1;
+    move() : Point {
+        if (this.pos.x + this.dir.x <= this.min.x) {
+            this.dir.x *= -1;
         }
-        if (this.posY + this.dirY <= this.minY) {
-            this.dirY *= -1;
+        if (this.pos.y + this.dir.y <= this.min.y) {
+            this.dir.y *= -1;
         }
-        if (this.posX + this.dirX + this.radius * 2 >= this.maxX) {
-            this.dirX *= -1;
+        if (this.pos.x + this.dir.x + this.radius * 2 >= this.max.x) {
+            this.dir.x *= -1;
         }
-        if (this.posY + this.dirY + this.radius * 2 >= this.maxY) {
-            this.dirY *= -1;
+        if (this.pos.y + this.dir.y + this.radius * 2 >= this.max.y) {
+            this.dir.y *= -1;
         }        
-        this.posX += this.dirX;
-        this.posY += this.dirY;        
 
-        return [this.posX, this.posY];
+        this.pos.add(this.dir);
+
+        return this.pos;
     }
 
     setConstraints(minX : number, minY : number, maxX : number, maxY : number) {
-        this.minX = minX;
-        this.minY = minY;
-        this.maxX = maxX;
-        this.maxY = maxY;
+        this.min = new Point(minX, minY);
+        this.max = new Point(maxX, maxY);
     }    
 }
 
@@ -54,7 +60,7 @@ var ball = new Ball(parseInt(getComputedStyle(ballElement)['border-top-left-radi
 ball.setConstraints(0, 0, boardElement.offsetWidth, boardElement.offsetHeight);
 
 setInterval(() => {
-    let [posX, posY] = ball.move();
+    let {x: posX, y: posY} = ball.move();
 
     ballElement.style.left = posX + 'px';
     ballElement.style.top = posY + 'px';        
