6eaff42171848bf6e25dbc4ce0fed6987a1e53c3
Łukasz Piotr Łuczak <lukasz.luczak@onet.pl>
2016-07-17 12:12:23 +0200

    [L02] Constrainted ball movement

--- app/application.ts
+++ app/application.ts
@@ -4,6 +4,10 @@ class Ball {
     posY : number;
     dirX : number;
     dirY : number; 
+    minX : number;
+    minY : number;
+    maxX : number;
+    maxY : number;
 
     constructor(radius : number, posX : number, posY : number, dirX : number, dirY : number) {
         this.radius = radius;
@@ -14,22 +18,44 @@ class Ball {
     }
 
     move() {
+        if (this.posX + this.dirX <= this.minX) {
+            this.dirX *= -1;
+        }
+        if (this.posY + this.dirY <= this.minY) {
+            this.dirY *= -1;
+        }
+        if (this.posX + this.dirX + this.radius * 2 >= this.maxX) {
+            this.dirX *= -1;
+        }
+        if (this.posY + this.dirY + this.radius * 2 >= this.maxY) {
+            this.dirY *= -1;
+        }        
         this.posX += this.dirX;
-        this.posY += this.dirY;
+        this.posY += this.dirY;        
 
         return [this.posX, this.posY];
     }
+
+    setConstraints(minX : number, minY : number, maxX : number, maxY : number) {
+        this.minX = minX;
+        this.minY = minY;
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }    
 }
 
 console.log('Hello from BrickBuster !!!');
 
-var ball = new Ball(16, 100, 100, 1, 1);
-
 var ballElement : HTMLElement = <HTMLElement>document.getElementsByClassName("ball")[0];
+var boardElement : HTMLElement = <HTMLElement>document.getElementsByClassName("game-board")[0];
+
+var ball = new Ball(parseInt(getComputedStyle(ballElement)['border-top-left-radius']), ballElement.offsetLeft, ballElement.offsetTop, 1, -1);
+
+ball.setConstraints(0, 0, boardElement.offsetWidth, boardElement.offsetHeight);
 
 setInterval(() => {
     let [posX, posY] = ball.move();
 
     ballElement.style.left = posX + 'px';
     ballElement.style.top = posY + 'px';        
-}, 50);
+}, 20);
